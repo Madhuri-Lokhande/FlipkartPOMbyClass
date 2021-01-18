957080ea281d22ab6eabcd95f173fb2054bffe6b
Madhuri-Lokhande <lokhande8madhuri@email.com>
2021-01-18 15:32:38 +0000

    Added homePageTest in FirstAttemptTowardsPOM Project

---
+++ src/com/flipkart/Test/HomePageTest.java
@@ -0,0 +1,8 @@
+package com.flipkart.Test;
+
+public class HomePageTest {
+
+	public void verifyFooter() {
+		System.out.println("Verify footer of the page (This is just to check on github)");
+	}
+}
